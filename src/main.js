55cca698d7b76a1b9d1904cf2cc38a3b1acb2d61
OksKuskova <oskanchik.kuskova@gmail.com>
2024-04-26 16:57:30 +0200

    добавит sorting-view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import FiltersView from './view/filters-view.js';
+import SortingView from './view/sorting-view.js';
 import { render } from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
+const sortingsContainer = document.querySelector('.trip-events');
 
 render(new FiltersView(), filtersContainer);
+render(new SortingView(), sortingsContainer);

d8055131cf682afd6ea82f01cf6efedeab7597a0
OksKuskova <oskanchik.kuskova@gmail.com>
2024-05-03 08:06:20 +0200

    Свяжет waypoint, destination и offers через id

--- src/mock/waypoints.js
+++ src/mock/waypoints.js
@@ -1,29 +1,49 @@
 import { generateId, getRandomInteger, getRandomArrayElement, flipCoin, createArray } from '../utils.js';
-import { typesOfWaypoint } from '../const.js';
-// import { getRandomDestination } from './destination.js';
-// import { getOffersForWaypoint } from './offers.js';
+import { typesOfWaypoint, dateFromList, dateToList } from '../const.js';
+import { getRandomDestination } from './destination.js';
+import { getOffersForWaypoint } from './offers.js';
 
+const getId = (element) => {
+  if (Array.isArray(element)) {
+    return element.map((value) => value.id);
+  } else {
+    return element.id;
+  }
+};
 
-// const getId = (element) => {
-//   if (Array.isArray(element)) {
-//     return element.map((value) => value.id);
-//   } else {
-//     return element.id;
-//   }
-// };
+const createWaypoint = () => {
+  const typeOfWaypoint = getRandomArrayElement(typesOfWaypoint);
+  // console.log(typeOfWaypoint);
+  const destinationOfWaypoint = getRandomDestination();
+  // console.log(destinationOfWaypoint);
+  const offersOfWaypoint = getOffersForWaypoint(typeOfWaypoint);
+  // console.log(offersOfWaypoint);
 
-const createWaypoint = () => ({
-  id: generateId(),
-  type: getRandomArrayElement(typesOfWaypoint),
-  basePrice: getRandomInteger(200, 1000),
-  dateFrom: new Date('May 10, 2024 10:25:00'),
-  dateTo: new Date('May 10, 2024 15:30:00'),
-  destination: '',
-  isFavorite: flipCoin(),
-  offers: [],
-});
+  return {
+    id: generateId(),
+    type: typeOfWaypoint,
+    basePrice: getRandomInteger(200, 1000),
+    dateFrom: new Date(getRandomArrayElement(dateFromList)),
+    dateTo: new Date(getRandomArrayElement(dateToList)),
+    destination: getId(destinationOfWaypoint),
+    isFavorite: flipCoin(),
+    offers: getId(offersOfWaypoint),
+  };
+};
+
+// const createWaypoint = () => ({
+//   id: generateId(),
+//   type: getRandomArrayElement(typesOfWaypoint),
+//   basePrice: getRandomInteger(200, 1000),
+//   dateFrom: new Date(getRandomArrayElement(dateFromList)),
+//   dateTo: new Date(getRandomArrayElement(dateToList)),
+//   destination: '',
+//   isFavorite: flipCoin(),
+//   offers: [],
+// });
 
 const mockWaypoints = createArray(8, createWaypoint);
+// console.log(mockWaypoints);
 
 const getRandomWaypoint = () => getRandomArrayElement(mockWaypoints);
 

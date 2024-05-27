acff8006f370d99a08c8ce8d94b1148b8e0bcb88
OksKuskova <oskanchik.kuskova@gmail.com>
2024-05-27 12:47:46 +0200

    Оптимизирует создание шаблона (createSortingTemplate) и добавит обработчик изменения типа сортировки

--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,36 +1,39 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { SortType, Prefix } from '../const.js';
 
 const createSortingTemplate = () => `
-  <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
-      <label class="trip-sort__btn" for="sort-day">Day</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--event">
-      <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-      <label class="trip-sort__btn" for="sort-event">Event</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--time">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-      <label class="trip-sort__btn" for="sort-time">Time</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
-      <label class="trip-sort__btn" for="sort-price">Price</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--offer">
-      <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-      <label class="trip-sort__btn" for="sort-offer">Offers</label>
-    </div>
+  <form class="trip-events__trip-sort trip-sort" action="#" method="get">
+  ${Object.values(SortType)
+    .map((type) => `
+    <div class="trip-sort__item  trip-sort__item--${type}">
+      <input id="${Prefix.SORT_ID}${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${type === SortType.DEFAULT ? 'checked' : ''} ${type === SortType.EVENT || type === SortType.OFFERS ? 'disabled' : ''}>
+      <label class="trip-sort__btn" for="${Prefix.SORT_ID}${type}">${type}</label>
+    </div>`).join('')}
   </form>
   `;
 
 export default class SortingView extends AbstractView {
+  #handleSortTypeChange = null;
+  #currentSortType = SortType.DEFAULT;
+
+  constructor(onSortTypeChange) {
+    super();
+    this.#handleSortTypeChange = onSortTypeChange;
+
+    this.element.addEventListener('change', this.#sortTypeChangeHandler);
+  }
+
   get template() {
     return createSortingTemplate();
   }
+
+  #sortTypeChangeHandler = (evt) => {
+    evt.preventDefault();
+    const selectedSortType = evt.target.id.replace(Prefix.SORT_ID, '');
+    if (this.#currentSortType === selectedSortType) {
+      return;
+    }
+    this.#handleSortTypeChange(selectedSortType);
+    this.#currentSortType = selectedSortType;
+  };
 }

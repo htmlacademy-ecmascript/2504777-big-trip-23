7085dc7f87aa5c2b8753c3354525207571a74562
OksKuskova <oskanchik.kuskova@gmail.com>
2024-05-27 14:44:58 +0200

    Реализует метод сортировки, свяжет сортировку и рендеринг задач

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -3,10 +3,13 @@ import EventListView from '../view/event-list-view.js';
 import SortingView from '../view/sorting-view.js';
 import WaypointPresenter from './waypoint-presenter.js';
 import ListEmptyView from '../view/list-empty-view.js';
+import { sortByDefault, sortByPrice, sortByTime } from '../utils/sort.js';
+import { SortType } from '../const.js';
 
 export default class EventPresenter {
   #eventContainer = null;
   #waypointsModel = null;
+  #sortsComponent = null;
 
   #eventListComponent = new EventListView();
 
@@ -22,21 +25,12 @@ export default class EventPresenter {
   }
 
   init() {
-    this.#eventWaypoints = [...this.#waypointsModel.waypoints];
+    this.#eventWaypoints = sortByDefault([...this.#waypointsModel.waypoints]);
     this.#destinations = [...this.#waypointsModel.destinations];
     this.#offers = [...this.#waypointsModel.offers];
 
     this.#renderEventsBoard();
   }
-
-  #handleWaypointChange = (updatedWaypoint) => {
-    this.#eventWaypoints = this.#eventWaypoints.map((waypoint) => waypoint.id === updatedWaypoint.id ? updatedWaypoint : waypoint);
-    this.#waypointPresenters.get(updatedWaypoint.id).init(updatedWaypoint, this.#destinations, this.#offers);
-  };
-
-  #handleModeChange = () => {
-    this.#waypointPresenters.forEach((presenter) => presenter.modeReset());
-  };
 
   #renderWaypoint(waypoint, destination, offers) {
     const waypointPresenter = new WaypointPresenter(
@@ -48,17 +42,56 @@ export default class EventPresenter {
     this.#waypointPresenters.set(waypoint.id, waypointPresenter);
   }
 
+  #renderSorts() {
+    this.#sortsComponent = new SortingView(this.#handleSortTypeChange);
+    render(this.#sortsComponent, this.#eventContainer);
+  }
+
+  #renderWaypointsList() {
+    render(this.#eventListComponent, this.#eventContainer);
+
+    for (const eventWaypoints of this.#eventWaypoints) {
+      this.#renderWaypoint(eventWaypoints, this.#destinations, this.#offers);
+    }
+  }
+
+  #clearWaypointsList() {
+    this.#waypointPresenters.forEach((presenter) => presenter.destroy());
+    this.#waypointPresenters.clear();
+  }
+
   #renderEventsBoard() {
     if (!this.#eventWaypoints.length) {
       render(new ListEmptyView(), this.#eventContainer);
       return;
     }
 
-    render(new SortingView(), this.#eventContainer);
-    render(this.#eventListComponent, this.#eventContainer);
-
-    for (let i = 0; i < this.#eventWaypoints.length; i++) {
-      this.#renderWaypoint(this.#eventWaypoints[i], this.#destinations, this.#offers);
-    }
+    this.#renderSorts();
+    this.#renderWaypointsList();
   }
+
+  #handleWaypointChange = (updatedWaypoint) => {
+    this.#eventWaypoints = this.#eventWaypoints.map((waypoint) => waypoint.id === updatedWaypoint.id ? updatedWaypoint : waypoint);
+    this.#waypointPresenters.get(updatedWaypoint.id).init(updatedWaypoint, this.#destinations, this.#offers);
+  };
+
+  #handleModeChange = () => {
+    this.#waypointPresenters.forEach((presenter) => presenter.modeReset());
+  };
+
+  #handleSortTypeChange = (type) => {
+    switch(type) {
+      case SortType.PRICE:
+        sortByPrice(this.#eventWaypoints);
+        break;
+      case SortType.TIME:
+        sortByTime(this.#eventWaypoints);
+        break;
+      case SortType.DEFAULT:
+        sortByDefault(this.#eventWaypoints);
+        break;
+    }
+    this.#clearWaypointsList();
+    this.#renderWaypointsList();
+  };
 }
